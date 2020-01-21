682d0cc6327946a8175a96fbd9084c0f267f444d
Davuluri <C048154@corp.cvs.com>
2020-01-20 22:48:35 -0500

    added repository code

--- src/main/java/com/rewards/calculator/service/RewardsCalculatorImpl.java
+++ src/main/java/com/rewards/calculator/service/RewardsCalculatorImpl.java
@@ -1,8 +1,11 @@
 package com.rewards.calculator.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rewards.calculator.entity.RewardsCalculatorEntity;
 import com.rewards.calculator.repository.RewardsCalculatorRepository;
 
 @Service
@@ -10,5 +13,12 @@ public class RewardsCalculatorImpl implements RewardsCalculatorService{
 	
 	@Autowired
 	RewardsCalculatorRepository rewardsCalculatorRepository;
+	
+	public List<RewardsCalculatorEntity> calculateRewards() {
+		List<RewardsCalculatorEntity> rewardCalculatorEntity = rewardsCalculatorRepository.findAll();
+		
+		return null;
+		
+	}
 
 }
